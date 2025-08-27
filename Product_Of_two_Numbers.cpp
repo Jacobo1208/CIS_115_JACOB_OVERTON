576275f5511418dca2b63024ada48015c7c03e4e
Jacobo1208 <overtonjl861@mail.sandhills.edu>
2025-08-27 11:25:39 -0400

    product commit

---
+++ Product_Of_two_Numbers.cpp
@@ -0,0 +1,22 @@
+//@author: Jacob Overton
+//@date: 8/27/2025
+//@purpose: This program prints the numbers 0 through 10
+
+//include iostream directive 
+#include <iostream>
+
+using namespace std; //use the standard namespace
+
+//define the main() function/method 
+int main()
+{
+int num1 = 5;
+int num2 = 10; 
+int product = num1 * num2;
+
+//using the std output and print the hello world string
+std::cout << product << std::endl; 
+
+
+
+}
