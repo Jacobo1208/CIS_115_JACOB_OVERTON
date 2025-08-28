5e3ccfef8de7f9b50cfbfb7cb3d5ebd94f3fd64c
Jacobo1208 <overtonjl861@mail.sandhills.edu>
2025-08-28 11:16:22 -0400

    few changes

--- C++LAB2/ForceCalculations.cpp
+++ C++LAB2/ForceCalculations.cpp
@@ -1,6 +1,6 @@
 //@author: Jacob Overton
 //@date: 8/27/2025
-//@purpose: This program prints the voltage output of the function
+//@purpose: This program prints force output of the function
 
 #include <iostream> // required for inputs and outputs
 using namespace std; //std library for cin/cout functions
--- C++LAB2/VoltageCalculations.cpp
+++ C++LAB2/VoltageCalculations.cpp
@@ -2,7 +2,7 @@
 
 //@author: Jacob Overton
 //@date: 8/27/2025
-//@purpose: This program prints the product/distance traveled 
+//@purpose: This program prints voltage calculations 
 
 #include <iostream> // required for inputs and outputs
 using namespace std; //std library for cin/cout functions
