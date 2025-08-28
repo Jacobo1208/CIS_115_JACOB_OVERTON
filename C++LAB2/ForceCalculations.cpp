0e7b2319c4dbe886c25dd3b3e6532ee6d33291f9
Jacobo1208 <overtonjl861@mail.sandhills.edu>
2025-08-28 08:06:10 -0400

    commit changes

--- C++LAB2/ForceCalculations.cpp
+++ C++LAB2/ForceCalculations.cpp
@@ -1,19 +1,19 @@
 //@author: Jacob Overton
 //@date: 8/27/2025
-//@purpose: This program prints the product/distance traveled 
+//@purpose: This program prints the voltage output of the function
 
 #include <iostream> // required for inputs and outputs
 using namespace std; //std library for cin/cout functions
 
+
 int main()
 {
 
-int I= 10; 
-int R= 2;
+int m= 10;         
+double a= 9.81;      //double is used to store a result in a double variable 
 
-int V= I * R ; // Voltage calculation 
+double F= m * a ; // Force calculation  
 
-cout << "The voltage is " << V << " volts (v)"  << endl;
-
+cout << "The force output is " << F << " Newtons (N)"  << endl;
 
 }
--- C++LAB2/VoltageCalculations.cpp
+++ C++LAB2/VoltageCalculations.cpp
@@ -1,20 +1,21 @@
+
+
 //@author: Jacob Overton
 //@date: 8/27/2025
-//@purpose: This program prints the voltage output of the function
+//@purpose: This program prints the product/distance traveled 
 
 #include <iostream> // required for inputs and outputs
 using namespace std; //std library for cin/cout functions
 
-
 int main()
 {
 
-int m= 10;         
-double a= 9.81;      //double is used to store a result in a double variable 
+int I= 10; 
+int R= 2;
 
-double F= m * a ; // Force calculation  
+int V= I * R ; // Voltage calculation 
 
-cout << "The force output is " << F << " Newtons (N)"  << endl;
+cout << "The voltage is " << V << " volts (v)"  << endl;
 
 
 }
